52050224bdd5277b4d92436119689050278ea649
Albertoolci <albertoolci@hotmail.com>
2026-08-25 17:51:17 +0200

    chore(bootloader): implemented  loading of the kernel without regarding its size (until 1.40 MB)
    Now the kernel size is calculated when compiled and injected into te bootloader. The bootloader calculates the tracks, cylinder and head adjustments needed. The load of the kernel is done in real mode.

--- Makefile
+++ Makefile
@@ -1,22 +1,27 @@
 # Global Makefile
 BUILD_DIR=build
-BOOTLOADER=$(BUILD_DIR)/bootloader/bootloader.o
-OS=$(BUILD_DIR)/os/sample.o
+BOOTLOADER_SRC=bootloader/bootloader.asm
+BOOTLOADER_BIN=$(BUILD_DIR)/bootloader/bootloader.o
+OS=$(BUILD_DIR)/os/kernel.bin
 DISK_IMG=disk.img
 
 all: bootdisk
 
 .PHONY: bootdisk bootloader os
 
-bootloader:
-	make -C bootloader
-
 os:
 	make -C os
 
+bootloader: os
+	mkdir -p $(dir $(BOOTLOADER_BIN))
+	$(eval KERNEL_SIZE := $(shell stat -c%s $(OS)))
+	$(eval SECTORS := $(shell echo $$(( ($(KERNEL_SIZE) + 511) / 512 )) ))
+	@echo "Kernel size: $(KERNEL_SIZE) bytes ($(SECTORS) sectors)"
+	nasm -f bin $(BOOTLOADER_SRC) -DSECTORS_TO_READ=$(SECTORS) -o $(BOOTLOADER_BIN)
+
 bootdisk: bootloader os
 	dd if=/dev/zero of=$(DISK_IMG) bs=512 count=2880
-	dd conv=notrunc if=$(BOOTLOADER) of=$(DISK_IMG) bs=512 count=1 seek=0
+	dd conv=notrunc if=$(BOOTLOADER_BIN) of=$(DISK_IMG) bs=512 count=1 seek=0
 	dd conv=notrunc if=$(OS) of=$(DISK_IMG) bs=512 seek=1
 
 qemu:
@@ -25,3 +30,4 @@ qemu:
 clean:
 	make -C bootloader clean
 	make -C os clean
+	rm -f $(DISK_IMG)
--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -6,7 +6,14 @@ bits 16     ; Set the processor to 16-bit mode (real mode)
 start: 
     jmp boot
 
-; %include "movcursor.asm" ; Include the movcursor.asm file
+; Constant defining the number of sectors to read from disk. This can be overridden by passing a value from the Makefile.
+%ifndef SECTORS_TO_READ
+    SECTORS_TO_READ equ 64  ; Value can be overridden by passing a value from the Makefile
+%endif
+
+; Standard 1.44 MB floppy disk geometry
+SECTORS_PER_TRACK  equ 18
+HEADS_PER_CYLINDER equ 2
 
 ;; constant and variable definitions (0ah = line feed, 0dh = carriage return, 0h = null terminator)
 msg db "Welcome to My Operating System!", 0ah, 0dh, 0h
@@ -22,25 +29,65 @@ boot:
     mov ss, ax
     mov sp, 0x7C00
 
-    ; Set the buffer address to 0x500
-    mov ax, 0x50
+    mov [boot_drive], dl    ; Save the boot drive provided by BIOS
 
-    ; Set the buffer
+    ; Set the buffer address to 0x0500 (0x0050:0x0000)
+    mov ax, 0x0050
     mov es, ax
-    xor bx, bx
+    xor bx, bx              ; BX always 0 for int 0x13
 
-    mov al, 15 ; read 15 sectors
-    mov ch, 0 ; track 0
-    mov cl, 2 ; sector to read (2)
-    mov dh, 0 ; head number (0)
+    mov cx, SECTORS_TO_READ ; Load defined sector count
+    mov si, 1               ; SI = Initial LBA sector (physical sector 2 on disk)
 
-    mov ah, 0x02 ; BIOS read sector function
-    int 0x13 ; call BIOS interrupt
-    jmp 0x50:0x0 ; jump and execute the sector
+read_loop:
+    push cx                 ; Preserve remaining sectors counter
+
+    mov ax, si              ; AX = Current LBA
+    call lba_to_chs         ; Convert AX to CHS (CL=sector, CH=cylinder, DH=head)
+
+    mov dl, [boot_drive]
+    mov ax, 0x0201          ; BIOS read sector function (AH=0x02, AL=1 sector)
+    int 0x13                ; call BIOS interrupt
+    jc disk_error           ; If disk error, jump to error handler
+
+    ; Advance the ES segment by 512 bytes (0x0020 in segment arithmetic)
+    mov ax, es
+    add ax, 0x0020
+    mov es, ax
+
+    inc si                  ; Next LBA sector
+    pop cx                  ; Restore sector counter
+    loop read_loop          ; Decrement CX and loop if CX > 0
+
+    sti                     ; Re-enable interrupts before jumping
+    jmp 0x0050:0x0000       ; jump and execute the sector
 
 disk_error:
     hlt ; halt the system
 
-; We have to be 512 bits. Clear the rest of the bytes with 0
+; Converts LBA (AX) -> CH (Cylinder), DH (Head), CL (Sector)
+lba_to_chs:
+    push bx
+    
+    xor dx, dx
+    mov bx, SECTORS_PER_TRACK
+    div bx                  ; AX = LBA / 18, DX = LBA % 18
+
+    inc dx                  ; Sectors are 1-based
+    mov cl, dl              ; CL = Sector (1-18)
+
+    xor dx, dx
+    mov bx, HEADS_PER_CYLINDER
+    div bx                  ; AX = Cylinder, DX = Head
+
+    mov ch, al              ; CH = Cylinder
+    mov dh, dl              ; DH = Head
+
+    pop bx
+    ret
+
+boot_drive db 0
+
+; We have to be 512 bytes. Clear the rest of the bytes with 0
 times 510 - ($ - $$) db 0 ; Fill the rest of the bootloader with zeros
 dw 0xAA55 ; Boot signature (must be 0xAA55 for BIOS to recognize it as a valid bootloader)
